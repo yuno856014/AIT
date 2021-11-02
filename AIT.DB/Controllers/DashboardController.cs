cd7a58c178686d9f152a4ca8cad5ef0f94cec6d4
aureole-it <sourcetree@aureole-it.vn>
2021-11-02 16:46:47 +0700

    up

---
+++ AIT.DB/Controllers/DashboardController.cs
@@ -0,0 +1,20 @@
+﻿using AIT.DB.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AIT.DB.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [Authorize(Roles = "SystemAdmin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
